953cdb2b50f2779f5680c2b904c7790f4916ceb0
agarciaro <aitorgarciarojado@gmail.com>
2022-05-05 21:53:16 +0200

    añadida getbalance BUSD

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import {Component} from '@angular/core';
-import {Web3Service} from "./services/contract/web3.service";
+import { Component } from '@angular/core';
+import { Web3Service } from "./services/contract/web3.service";
 
 @Component({
   selector: 'app-root',
@@ -9,18 +9,26 @@ import {Web3Service} from "./services/contract/web3.service";
 export class AppComponent {
   authenticated: boolean = false;
   data: string[] | undefined;
+  balance: string | undefined;
 
 
-  constructor(
-    private web3: Web3Service) {
+  constructor(private web3: Web3Service) {
   }
 
 
   Connect() {
     this.web3.connectAccount().then(response => {
       console.log(response);
-      this.data = response
-    })
+      this.data = response;
+      if (this.data !== undefined) {
+        this.web3.accountInfo(this.data[0]).then(info => {
+          console.log(this.web3.balance);
+        })
+      }
+
+    });
+
+
   }
 
 }
